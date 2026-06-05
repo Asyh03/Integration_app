1982f903a7bfe8815fee978fdffe69e00cb727c2
AHETO Samuel <samuelaheto02@gmail.com>
2026-06-05 07:49:35 +0000

    fiche d'un nouveau fonctionnelle

---
+++ pages/4_Fiche_nouveau.py
@@ -0,0 +1,39 @@
+import streamlit as st
+from database import supabase
+
+st.title("Fiche d'un nouveau")
+
+#Récupérer tous les nouveaux
+data = supabase.table("nouveaux").select("*").execute().data
+
+if not data :
+    st.info("Aucune personne enregistrée;")
+else :
+    #Menu de selection
+    noms = {f"{n['prenom']} {n['nom']}": n for n in data}
+    choix = st.selectbox("Choisir une personne", list(noms.keys()))
+    personne = noms[choix]
+
+    #Afficher la fiche
+    col1, col2 = st.columns(2)
+
+    with col1 :
+        st.subheader("Informations personnelles")
+        st.write(f"**Nom:** {personne['nom']}")
+        st.write(f"**Prénom :** {personne['prenom']}")
+        st.write(f"**Téléphone :** {personne['telephone']}")
+        st.write(f"**Tranche d'âge :** {personne['tranche_age']}")
+        st.write(f"**Invité par :** {personne['invite_par']}")
+        st.write(f"**Date de visite :** {personne['date_visite']}")
+    with col2 :
+        st.subheader("Suivi")
+        st.write(f"**Statut :** {personne['statut']}")
+
+        #Changer le statut
+        statuts = ["Tous","Nouveau","Contacté", "Revient régulièrement", "Intégré","Inactif"]
+        nouveau_statut = st.selectbox("Changer el statut", statuts, index = statuts.index(personne['statut']))
+
+        if st.button("Mettre à jour le statut"):
+            supabase.table("nouveaux").update({"statut": nouveau_statut}).eq("id", personne["id"]).execute()
+            st.success("Statut mis à jour ! ✅")
+            st.rerun()
